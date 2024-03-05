4544fe03dfab79f884ad713dd6233171b7de0f55
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-03-05 21:43:34 +0900

    リスト5.29まで学習完了

---
+++ chap05/array_search_bad.php
@@ -0,0 +1,9 @@
+<?php
+$data = ['PHP', 'JavaScript', 'PHP', 'Java', 'C#', '15'];
+if (!array_search('PHP', $data)) {
+    print "見つかりませんでした";
+}
+
+if (!in_array('PHP', $data)) {
+    print "見つかりませんでした";
+}
