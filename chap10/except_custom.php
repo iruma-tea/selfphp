f779bf76d23fbb13ec21ad43683b78dec735bc7d
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-03-14 21:33:40 +0900

    10.7 例外処理の学習完了

---
+++ chap10/except_custom.php
@@ -0,0 +1,11 @@
+<?php
+interface MyAppException
+{
+}
+
+class MyInputException extends RuntimeException implements MyAppException
+{
+}
+class MyOutputException extends RuntimeException implements MyAppException
+{
+}
---
+++ chap10/except_custom2.php
@@ -0,0 +1,18 @@
+<?php
+require_once __DIR__ . "/except_custom.php";
+
+class UserNotFoundException extends Exception implements MyAppException
+{
+    protected string $userid;
+
+    public function __construct(string $userid, int $code = 0, Throwable $previous = null)
+    {
+        parent::__construct("{$userid}が存在しません。", $code, $previous);
+        $this->userid = $userid;
+    }
+
+    final public function getUserid(): string
+    {
+        return $this->getUserid();
+    }
+}
